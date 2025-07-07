c38166416cc2ac4f992e0e53291950a3dc338701
dev_local <devanshusingh55da@gmail.com>
2025-07-07 19:00:11 +0530

    commit

--- CompanyEmployees/Repository/RepositoryContext.cs
+++ CompanyEmployees/Repository/RepositoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,13 @@ namespace Repository
     {
         public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options) { }
 
-        public DbSet<Company> Companies { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new CompanyConfiguration()); 
+			modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+		}
+
+		public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
     }
 }
