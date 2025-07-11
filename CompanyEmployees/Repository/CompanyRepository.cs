f8de4b4a3f6819e2d1f1b0443b096a5dcbcfe4cf
dev_local <devanshusingh55da@gmail.com>
2025-07-11 17:17:48 +0530

    Commit

--- CompanyEmployees/Repository/CompanyRepository.cs
+++ CompanyEmployees/Repository/CompanyRepository.cs
@@ -8,8 +8,12 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-	public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+	internal sealed class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
 	{
 		public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext){ }
+
+		public IEnumerable<Company> GetAllCompanies(bool trackChanges) => 
+			FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+
 	}
 }
